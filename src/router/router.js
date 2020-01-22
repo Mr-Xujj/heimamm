4b65ae99981c296a2875d49ce0a93b0f60d570eb
Mr-Xujj <XUJJ13071637518@outlook.com>
2020-01-22 22:23:47 +0800

    登录模块 - 路由整合

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,9 @@
 import Vue from 'vue'
 // 导入 vue-router
 import VueRouter from 'vue-router'
+
+//  导入登录组件
+import login from "../views/login/login.vue"
 //注册 vue-router
 Vue.use(VueRouter)
 // 实例化
@@ -9,8 +12,8 @@ const router = new VueRouter({
 //这里就是路由的配制项
 routes: [
  {
-path: '/这里可给路由地址一个名字',
-component: 0 
+path: '/login',
+component: login
  }
   ]
 })
