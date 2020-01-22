df1a400f37977a3c352d3e3701a98b8231b897e6
Mr-Xujj <XUJJ13071637518@outlook.com>
2020-01-22 23:06:34 +0800

    登录模块 - 标题区域布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="login-box">
-    <div class="form-box"></div>
+    <div class="form-box">
+      <div class="title-box">
+        <img src="../../assets/矢量智能对象 拷贝 9.png" alt />
+        <span class="title">黑马面面</span>
+        <i></i>
+        <span class="sub-title">用户登录</span>
+      </div>
+    </div>
     <img class="login-pic" src="../../assets/login_banner_ele.png" alt />
   </div>
 </template>
@@ -16,13 +23,42 @@ export default {
   display: flex;
   align-items: center;
   justify-content: space-around;
-  background:linear-gradient(225deg,rgba(20,147,250,1),rgba(1,198,250,1));
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
   height: 100%;
   .form-box {
     width: 478px;
     height: 550px;
     background: rgba(245, 245, 245, 1);
+    padding: 44px;
+    box-sizing: border-box;
+    .title-box {
+      display: flex;
+      align-items: center;
+      img {
+        width: 22px;
+        height: 17px;
+        margin-right: 16px;
+      }
+      .title {
+        font-size: 24px;
+        margin-right: 14px;
+      }
+      i {
+        width: 1px;
+        height: 28px;
+        background: rgba(199, 199, 199, 1);
+        margin-right: 12px;
+      }
+      .sub-title {
+        font-size: 22px;
+        font-weight: 400;
+        color: rgba(12, 12, 12, 1);
+      }
+    }
   }
-
 }
 </style>
