3bcb4ed12f74764bdbef3b3400ad5770596cca73
Mr-Xujj <XUJJ13071637518@outlook.com>
2020-01-22 22:52:31 +0800

    登录模块 - 整体布局

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router  from './router/router.js'
 //  导入饿了么
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+//  导入全局样式
+import '../src/style/base.css'
 // 注册
 Vue.use(ElementUI);
 
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,13 +1,28 @@
 <template>
-  <div>登录</div>
+  <div class="login-box">
+    <div class="form-box"></div>
+    <img class="login-pic" src="../../assets/login_banner_ele.png" alt />
+  </div>
 </template>
 
 <script>
 export default {
-name:"app"
-}
+  name: "app"
+};
 </script>
 
-<style>
+<style lang="less">
+.login-box {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background:linear-gradient(225deg,rgba(20,147,250,1),rgba(1,198,250,1));
+  height: 100%;
+  .form-box {
+    width: 478px;
+    height: 550px;
+    background: rgba(245, 245, 245, 1);
+  }
 
+}
 </style>
