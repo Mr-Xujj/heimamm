fe2a916fef11cdd393462c0a7d7b9aa120307d5f
Mr-Xujj <XUJJ13071637518@outlook.com>
2020-01-23 16:32:19 +0800

    登录模块 - 整合表单验证

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -13,31 +13,36 @@
         :model="ruleForm"
         :rules="rules"
         ref="ruleForm"
-        label-width="100px"
-        class="demo-ruleForm" >
+        class="login-form" >
 
-        <el-form-item  prop="name">
-          <el-input v-model="ruleForm.name"></el-input>
+        <el-form-item  prop="phone" >
+          <el-input v-model="ruleForm.phone" placeholder="请输入手机号"  prefix-icon="el-icon-user"></el-input>
         </el-form-item>
 
-        <el-form-item  prop="name">
-          <el-input v-model="ruleForm.name"></el-input>
-        </el-form-item>
-        
-        <el-form-item  prop="name">
-          <el-input v-model="ruleForm.name"></el-input>
+        <el-form-item  prop="password">
+          <el-input v-model="ruleForm.password" placeholder="请输入密码" show-password prefix-icon="el-icon-lock"></el-input>
         </el-form-item>
 
+        <el-form-item  prop="captcha">
+          <el-row>
+            <el-col :span="18">
+          <el-input v-model="ruleForm.captcha" placeholder="请输入验证码"  prefix-icon="el-icon-key"></el-input>
+            </el-col>
+            <el-col :span="6">
+              <img class="captcha" src="../../assets/captcha.png" alt="">
+            </el-col>
+          </el-row>
+        </el-form-item>
 
-        <el-form-item  prop="type">
-          <el-checkbox-group v-model="ruleForm.type">
-            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-          </el-checkbox-group>
+        <el-form-item>
+          <el-checkbox v-model="ruleForm.checked">
+            我已同意阅读<el-link type="primary"> 用户协议</el-link> 和 <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
         </el-form-item>
        
         <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-          <el-button @click="resetForm('ruleForm')">重置</el-button>
+          <el-button type="primary" @click="submForm" >登录</el-button>
+          <el-button class="register-button" type="success">注册</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -48,68 +53,60 @@
 
 <script>
 export default {
-  name: "app",
+  name: "login",
   data() {
+    //  自定义验证规则
+    var checkphone = (rule,value,callback)=>{
+      if(!value){
+        return callback(new Error("手机号不能为空"))
+      }else{
+        const reg  =/^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/
+        if(reg.test(value) == true){
+          callback();
+        }else{
+          callback(new Error("请输入正确手机号"))
+        }
+      }
+    }
     return {
       ruleForm: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: ""
+        phone: "",
+        password: "",
+        captcha:"",
+        checked: false,
+      
       },
       rules: {
-        name: [
-          { required: true, message: "请输入活动名称", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        phone: [
+          { required: true, validator:checkphone, trigger: "blur" },
         ],
-        region: [
-          { required: true, message: "请选择活动区域", trigger: "change" }
+        password: [
+          { required: true, message: "密码不能为空", trigger: "change" },
+          { min: 6, max: 18, message: "长度在 6 到 18 个字符", trigger: "change" }
         ],
-        date1: [
-          {
-            type: "date",
-            required: true,
-            message: "请选择日期",
-            trigger: "change"
-          }
-        ],
-        date2: [
-          {
-            type: "date",
-            required: true,
-            message: "请选择时间",
-            trigger: "change"
-          }
-        ],
-        type: [
-          {
-            type: "array",
-            required: true,
-            message: "请至少选择一个活动性质",
-            trigger: "change"
-          }
-        ],
-        resource: [
-          { required: true, message: "请选择活动资源", trigger: "change" }
-        ],
-        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }]
+        captcha: [
+          { required: true, message: "验证码不能为空", trigger: "change" },
+          { min: 4, max: 4, message: "验证码长度为4个字符", trigger: "change" }
+        ]
+       
+       
       }
     };
   },
   methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          window.console.log("error submit!!");
-          return false;
-        }
-      });
+    submForm() {
+       if(this.ruleForm.checked ===false){
+         this.$message.warning("勾选用户协议")
+       }else{
+         this.$refs.ruleForm.validate(valid => {
+           if (valid) {
+             alert("submit!");
+           } else {
+             window.console.log("error submit!!");
+             return false;
+           }
+         });
+       }
     },
     resetForm(formName) {
       this.$refs[formName].resetFields();
@@ -157,6 +154,28 @@ export default {
         font-size: 22px;
         font-weight: 400;
         color: rgba(12, 12, 12, 1);
+      }
+    }
+    .login-form{
+      margin-top: 30px;
+      .captcha{
+        height: 40px;
+        width: 100%;
+      }
+      .el-checkbox{
+        display: flex;
+        align-items: center;
+        .el-checkbox__label{
+          display: flex;
+          align-items: center
+        }
+      }
+      .el-button{
+        width: 100%;
+        margin: 0;
+      }
+      .register-button{
+        margin-top: 30px;
       }
     }
   }
